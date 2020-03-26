39d7ea3db4dd1e39de52b7c26fae8ba753af742c
Morgan Webb <mnbwebb2233@gmail.com>
2020-03-26 13:58:51 +1100

    Sync

--- modules/encryption.rb
+++ modules/encryption.rb
@@ -9,19 +9,19 @@ class Encryption
         return Gibberish::AES.new(passphrase).decrypt(encrypted_data)
     end
 
-    def write_to_file(encrypted_data)
+    def write_file(encrypted_data)
         # create new file and write encrypted data
         file = File.open("save", "w+") { |f|
             f.write(encrypted_data)
         }
     end
 
-    def load_file()
+    def read_file()
         return File.read("save")
     end
 end
 
 encryption = Encryption.new()
 encrypted_data = encryption.encrypt_data("Hello, World!", "Test123")
-encryption.write_to_file(encrypted_data)
-p decrypted_data = encryption.decrypt_data(encryption.load_file(), "Test123")
+encryption.write_file(encrypted_data)
+p decrypted_data = encryption.decrypt_data(encryption.read_file(), "Test123")
